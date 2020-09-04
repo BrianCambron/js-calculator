a19010f08a0aec8aa62eb91bcff57f84f460259a
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-04 12:06:43 -0400

    added the function named pushNumber that alerts

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 window.onload = function functionAlert(){
   alert("hello");
 }
+function pushNumber(event){
+  alert(event.target.value);
+}
+const buttonElements = document.querySelectorAll('button.number');
+buttonElements.forEach(function(button){
+button.addEventListener ('click', pushNumber);
+});
