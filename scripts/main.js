70ee232274eb46de919225df39a1fae21a0ca79f
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-04 12:41:27 -0400

    added function named pushOperator that alerts

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 window.onload = function functionAlert(){
-  alert("hello");
+  alert("hello"); //<- alerts hello onload
 }
 function pushNumber(event){
   alert(event.target.value);
@@ -7,4 +7,16 @@ function pushNumber(event){
 const buttonElements = document.querySelectorAll('button.number');
 buttonElements.forEach(function(button){
 button.addEventListener ('click', pushNumber);
+});
+
+function pushOperator(event){
+alert(event.target.value);
+}
+const operatorElements = document.querySelectorAll('button.operator', 'button.clear');
+const clearElement = document.querySelectorAll('button.clear')
+operatorElements.forEach(function(operator) {
+operator.addEventListener('click', pushOperator);
+clearElement.forEach(function(clear){
+clear.addEventListener('click', pushOperator);
+})
 });
